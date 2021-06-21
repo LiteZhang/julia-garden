ffd942f04b835f127d537037d4ead4fb79f0a2d1
lite zhang <lite@wirush.ai>
2021-06-21 16:37:54 +0800

    add the svg icon component

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+// eslint-disable-next-line no-unused-vars
+function resolve(dir) {
+  return path.join(__dirname, ".", dir);
+}
+
+module.exports = {
+  lintOnSave: false,
+  publicPath: "./",
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule("svg");
+    svgRule.uses.clear();
+    svgRule
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader")
+      .options({
+        symbolId: "icon-[name]",
+      })
+      .end()
+      .include.add(path.join(__dirname, "src/icons"))
+      .end();
+  },
+};
