6f449aadcab53a6a56238d83cc5e2e4821e358ab
lite <lite_zhang@163.com>
2021-08-09 09:33:47 +0800

    for the height of different screen  ver 0.9

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import router from "./router";
 import "@/icons";
 import Element from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
+
+import axios from 'axios'
 // Vue.use(ElementUI)
 // router.beforeEach((to, from, next) => {
 //   if (to.meta.title) {
@@ -16,4 +18,8 @@ import 'element-plus/lib/theme-chalk/index.css'
 const app = createApp(App).use(router);
 app.mount("#app");
 app.use(Element);
+app.use(axios)
+
+//Vue.prototype.$axios = axios;
+
 
